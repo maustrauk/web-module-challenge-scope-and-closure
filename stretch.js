f30ec866aa1e993c029ec84510909acfd944ff7e
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-12 15:09:36 -0400

    mvp 3.2

--- stretch.js
+++ stretch.js
@@ -4,3 +4,16 @@
 
   console.log("a defined? " + (typeof a !== 'undefined'));//a is undefined, because it try to put variable b (that didn't defined yet) in a.
   console.log("b defined? " + (typeof b !== 'undefined'));//b is defined as 3.
+
+
+
+function createBase(x) {
+    return function (y) {
+        return x + y;
+    }
+}
+
+var addSix = createBase(6);
+
+console.log(addSix(10));//16
+console.log(addSix(21));//27
