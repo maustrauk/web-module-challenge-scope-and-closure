5480316552b34012019e398eb0deed50f8bede43
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-12 11:58:44 -0400

    task 1-2

--- index.js
+++ index.js
@@ -56,10 +56,8 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(){
+  return Math.floor(Math.random()*2);
 }
 
 /* Task 3: finalScore()
